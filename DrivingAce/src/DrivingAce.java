14d219e95d0d505c756212fc1b3b3b44626b1baa
Jerry Zhu <jerryzhu509@gmail.com>
2019-05-24 10:57:50 -0400

    lul

--- DrivingAce/src/DrivingAce.java
+++ DrivingAce/src/DrivingAce.java
@@ -1,22 +1,29 @@
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.*;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
 /**
- * @author Jerry Zhu
+ * @author Nicholas Glenn and Jerry Zhu
  *
  */
 public class DrivingAce extends Application {
   private AnchorPane root; 
   
   @Override
-  public void start(Stage primaryStage) {
+  public void start(Stage primaryStage) throws InterruptedException {
     primaryStage.setResizable(false);
     root = new AnchorPane();
     root.setPrefSize(700, 500);
@@ -31,13 +38,61 @@ public class DrivingAce extends Application {
     
     primaryStage.setTitle("Driving Ace");
     primaryStage.setScene(new Scene(root));
-    MainMenu menu = new MainMenu(root);
+    mainMenu();
     
     primaryStage.show();
   }
+  
+  public void intro() {
+    
+  }
 
+  
+  public void mainMenu() {
+    MenuButton playBtn = new MenuButton("New Game");
+    playBtn.setLayoutX(100);
+    playBtn.setLayoutY(100);
+    root.getChildren().add(playBtn);
+    
+    playBtn.setOnAction(e -> levelOne());
+    
+    MenuButton helpBtn = new MenuButton("Help");
+    helpBtn.setLayoutX(100);
+    helpBtn.setLayoutY(200);
+    root.getChildren().add(helpBtn);
+    
+    helpBtn.setOnAction(e -> instructions());
+    
+    MenuButton exitBtn = new MenuButton("Quit");
+    exitBtn.setLayoutX(100);
+    exitBtn.setLayoutY(300);
+    root.getChildren().add(exitBtn);
+    
+    exitBtn.setOnAction(e -> Platform.exit());
+    
+    BackgroundImage background = new BackgroundImage(new Image("/resources/menubackground.jpg",200,515,false,true),
+        BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+        BackgroundSize.DEFAULT);
+    root.setBackground(new Background(background));
+  }
+  
+  public void instructions() {
+    
+  }
+  
+  public void levelOne() {
+    
+  }
+  
+  public void levelTwo() {
+    
+  }
+  
+  public void levelThree() {
+    
+  }
+  
   public static void main(String[] args) {
     launch(args);
   }
-
 }
--- DrivingAce/src/MainMenu.java
+++ DrivingAce/src/MainMenu.java
@@ -21,30 +21,6 @@ public class MainMenu {
   private long startNanoTime;
   
   public MainMenu(AnchorPane r) {
-    MenuButton playBtn = new MenuButton("New Game");
-    playBtn.setLayoutX(100);
-    playBtn.setLayoutY(100);
-    r.getChildren().add(playBtn);
-    
-    playBtn.setOnAction(e -> new LevelOne(r));
-    
-    MenuButton helpBtn = new MenuButton("Help");
-    helpBtn.setLayoutX(100);
-    helpBtn.setLayoutY(200);
-    r.getChildren().add(helpBtn);
-    
-    helpBtn.setOnAction(e -> new Instructions(r));
-    
-    MenuButton exitBtn = new MenuButton("Quit");
-    exitBtn.setLayoutX(100);
-    exitBtn.setLayoutY(300);
-    r.getChildren().add(exitBtn);
-    
-    exitBtn.setOnAction(e -> Platform.exit());
-    
-    BackgroundImage background = new BackgroundImage(new Image("/resources/menubackground.jpg",200,515,false,true),
-        BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
-        BackgroundSize.DEFAULT);
-    r.setBackground(new Background(background));
+
   }
 }
