dce7af9faec634a3f25c07c4eb419111129ce5fa
Jerry Zhu <jerryzhu509@gmail.com>
2019-05-30 20:52:43 -0400

    l

--- DrivingAce/src/Pylon.java
+++ DrivingAce/src/Pylon.java
@@ -1,10 +1,12 @@
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 
 public class Pylon extends Obstacle {
 
   public Pylon(double x, double y) {
-    super(x, y, 10, 10);
-    setFill(Color.ORANGE);
+    super(x, y, 20, 20);
+    setFill(new ImagePattern(new Image("/resources/cone_straight.png")));
   }
 
 }
